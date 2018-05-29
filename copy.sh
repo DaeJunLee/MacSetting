5721b9b872df759b2226afaa9330ae7f67240a34
DaeJun <dejavudev@gmail.com>
2018-05-29 10:54:30 +0900

    [Updated]Change path

--- copy.sh
+++ copy.sh
@@ -3,7 +3,7 @@
 ## About Xcode
 # Copy Xcode keyboard shortcut
 echo '[START]Copy IDETextKeyBindingSet.plist'
-sudo cp -R /Users/jun/Documents/12.Mac\ setting\ file/Xcode\ keyboard\ shortcut/IDETextKeyBindingSet.plist /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Resources
+sudo cp -R ./IDETextKeyBindingSet.plist /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Resources
 
 # If file copy success
 if [ $? -eq 0 ]; then
